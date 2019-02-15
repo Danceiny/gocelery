beea8bb2d5920e3a6b5e048940a6674e786b689c
Danceiny <danceiny@gmail.com>
2019-02-15 16:33:45 +0800

    fix uuid version issue

--- uuid.go
+++ uuid.go
@@ -1,9 +1,9 @@
 package gocelery
 
-import "github.com/satori/go.uuid"
+import "github.com/Danceiny/go.uuid"
 
 // generateUUID generates a v4 uuid and returns it as a string
 func generateUUID() string {
     uuid4, _ := uuid.NewV4()
-    return uuid4.String();
+    return uuid4.String()
 }
